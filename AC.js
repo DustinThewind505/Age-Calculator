e7e48ac7767f4e2e43a543c4f910e1642a5633e1
Dustin Guillen <42304513+DustinThewind505@users.noreply.github.com>
2019-03-31 22:45:57 -0500

    Create AC.js

---
+++ AC.js
@@ -0,0 +1,5 @@
+var yearDay = 365;
+var yearOld = prompt("How old are you?");
+var daysOld = yearDay * yearOld;
+var nextYear = yearDay + daysOld;
+alert("You'll be freaking " + nextYear + " days old on your next birthday.");
