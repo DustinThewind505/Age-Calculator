506a94854d72ebd1458049c111048de628439c82
unknown <Dustin.Guillen14@yahoo.com>
2020-05-29 16:56:41 -0600

    made express server, added expres knex sqlite3 helmet and express

--- Data/db-helper.js
+++
@@ -1,9 +0,0 @@
-const db = require('./db-config');
-
-// db.select('*').from('users');
-
-db('users').insert({ name: "Leo", age: 99 });
-
-db('users').where({id: 1}).update({ age: 88 });
-
-db('users').where({id: 1}).delete()
