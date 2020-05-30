6bf31051b021e62d56973f40a28ab028144e3e47
unknown <Dustin.Guillen14@yahoo.com>
2020-05-30 11:28:33 -0600

    added exress.Router

--- API/server.js
+++ API/server.js
@@ -1,61 +1,14 @@
 const express = require('express');
 
-const server = express();
-server.use(express.json())
+const hobbitsRouter = require('../hobbits/hobbitsRouter');
 
-let hobbits = [
-    {
-      id: 1,
-      name: 'Bilbo Baggins',
-      age: 111,
-    },
-    {
-      id: 2,
-      name: 'Frodo Baggins',
-      age: 33,
-    },
-  ];
-  let nextId = 3;
+const server = express();
+server.use(express.json());
+
+server.use('/hobbits', hobbitsRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'Ello Poppet'})
-})
-
-server.get('/ello', (req, res) => {
-    res.status(200).json(hobbits)
-})
-
-server.get('/ello/:id', (req, res) => {
-const hobbit = hobbits.find(h => h.id == req.params.id)
-
-    res.status(200).json(hobbit)
-})
-
-server.post('/ello', (req, res) => {
-    const hobbit = req.body;
-
-    hobbit.id = nextId++;
-
-    hobbits.push(hobbit);
-
-    res.status(201).json(hobbits)
-})
-
-server.put('/ello/:id', (req, res) => {
-    const hobbit = hobbits.find(h => h.id == req.params.id)
-
-    if(!hobbit){
-        res.status(404).json({message: "There is no Hobbit by that id"})
-    } else {
-    Object.assign(hobbit, req.body);
-    res.status(200).json(hobbit)
-    }
-})
-
-server.delete('/ello/:id', (req, res) => {
-
-    res.status(200).json({message: "DELETE"})
-})
-
+});
 
 module.exports = server;
---
+++ hobbits/hobbitsRouter.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const router = express.Router();
+
+let hobbits = [
+    {
+      id: 1,
+      name: 'Bilbo Baggins',
+      age: 111,
+    },
+    {
+      id: 2,
+      name: 'Frodo Baggins',
+      age: 33,
+    },
+  ];
+  let nextId = 3;
+
+
+router.get('/', (req, res) => {
+    res.status(200).json(hobbits)
+})
+
+router.get('/:id', (req, res) => {
+const hobbit = hobbits.find(h => h.id == req.params.id)
+
+    res.status(200).json(hobbit)
+})
+
+router.post('/', (req, res) => {
+    const hobbit = req.body;
+
+    hobbit.id = nextId++;
+
+    hobbits.push(hobbit);
+
+    res.status(201).json(hobbits)
+})
+
+router.put('/:id', (req, res) => {
+    const hobbit = hobbits.find(h => h.id == req.params.id)
+
+    if(!hobbit){
+        res.status(404).json({message: "There is no Hobbit by that id"})
+    } else {
+    Object.assign(hobbit, req.body);
+    res.status(200).json(hobbit)
+    }
+})
+
+router.delete('/:id', (req, res) => {
+
+    res.status(200).json({message: "DELETE"})
+})
+
+
+
+module.exports = router;
