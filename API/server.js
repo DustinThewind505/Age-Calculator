c93068c64ca3948a83477579be7b5cd6d8c4e942
unknown <Dustin.Guillen14@yahoo.com>
2020-05-30 00:16:00 -0600

    working on basic CRUD operations

--- API/server.js
+++ API/server.js
@@ -3,12 +3,45 @@ const express = require('express');
 const server = express();
 server.use(express.json())
 
+let hobbits = [
+    {
+      id: 1,
+      name: 'Bilbo Baggins',
+      age: 111,
+    },
+    {
+      id: 2,
+      name: 'Frodo Baggins',
+      age: 33,
+    },
+  ];
+  let nextId = 3;
+
 server.get('/', (req, res) => {
     res.status(200).json({message: 'Ello Poppet'})
 })
 
 server.get('/ello', (req, res) => {
-    res.status(200).send('<h1>Ello Poppet</h1>')
+    res.status(200).json(hobbits)
 })
+
+server.post('/ello', (req, res) => {
+    const hobbit = req.body;
+
+    hobbit.id = nextId++;
+
+    hobbits.push(hobbit);
+
+    res.status(201).json(hobbits)
+})
+
+server.put('/ello', (req, res) => {
+    res.status(201).json({message: "PUT Hobbits"})
+})
+
+server.delete('/ello', (req, res) => {
+    res.status(200).json({message: "DELETE"})
+})
+
 
 module.exports = server;
