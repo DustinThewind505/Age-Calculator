447b955c3cb9b54c6d07fc5d7c5560afa67bc7cb
unknown <Dustin.Guillen14@yahoo.com>
2020-05-31 12:47:10 -0600

    made custom 404 middleware with server.use

--- API/server.js
+++ API/server.js
@@ -7,8 +7,8 @@ server.use(express.json());
 
 server.use('/api', apiRoutes);
 
-server.get('/', (req, res) => {
-    res.send('<img src="http://3.bp.blogspot.com/-nY7RCflMJOk/TdVR-JHjEyI/AAAAAAAAAC8/D0tVTHeksow/s1600/Powerman_5000_umvd01.jpg"/>');
+server.use('/', (req, res) => {
+    res.send('<div style="padding:15% 0 5%;background-color:black;display: flex;flex-direction: column;align-items:center;"><h1 style="color:lawngreen;font-size:46px;">404 could not find page</h1><img src="http://3.bp.blogspot.com/-nY7RCflMJOk/TdVR-JHjEyI/AAAAAAAAAC8/D0tVTHeksow/s1600/Powerman_5000_umvd01.jpg"/></div>');
 });
 
 module.exports = server;
