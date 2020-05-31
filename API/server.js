97643bfd70ea7e55c6768ede4066e6881dd8bceb
unknown <Dustin.Guillen14@yahoo.com>
2020-05-31 14:00:39 -0600

    added third party middleware "morgan"

--- API/server.js
+++ API/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+
 
 const apiRoutes = require('./apiRoutes');
 
 const server = express();
 server.use(express.json());
+server.use(morgan('dev'));
 
 server.use('/api', apiRoutes);
 
 server.use('/', (req, res) => {
-    res.send('<div style="padding:15% 0 5%;background-color:black;display: flex;flex-direction: column;align-items:center;"><h1 style="color:lawngreen;font-size:46px;">404 could not find page</h1><img src="http://3.bp.blogspot.com/-nY7RCflMJOk/TdVR-JHjEyI/AAAAAAAAAC8/D0tVTHeksow/s1600/Powerman_5000_umvd01.jpg"/></div>');
+    res.status(404).send('<div style="padding:15% 0 5%;background-color:black;display: flex;flex-direction: column;align-items:center;"><h1 style="color:lawngreen;font-size:46px;">404 could not find page</h1><img src="http://3.bp.blogspot.com/-nY7RCflMJOk/TdVR-JHjEyI/AAAAAAAAAC8/D0tVTHeksow/s1600/Powerman_5000_umvd01.jpg"/></div>');
 });
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-
-
-
-
 const server = require('./api/server');
-const PORT = 4444;
 
+const PORT = 4444;
 
 server.listen(PORT, () => {
     console.log(`\n\t\t\t\t\t *** API running on localhost:${PORT} *** \n`)
