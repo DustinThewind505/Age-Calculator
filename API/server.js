7e00aa946b22e7e7cefde6efeb102ea099f9d51f
unknown <Dustin.Guillen14@yahoo.com>
2020-06-20 10:10:43 -0600

    added "path" dependency

--- API/server.js
+++ API/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 
 const apiRoutes = require('./apiRoutes');
@@ -7,13 +8,18 @@ const apiRoutes = require('./apiRoutes');
 const server = express();
 server.use(express.json());
 server.use(morgan('dev'));
-server.use(logger);
+// server.use(logger);
 
 
 server.use('/api', apiRoutes);
 
-server.use('/', (req, res) => {
-    res.status(404).send('<div style="padding:15% 0 5%;background-color:black;display: flex;flex-direction: column;align-items:center;"><h1 style="color:lawngreen;font-size:46px;">404 could not find page</h1><img src="http://3.bp.blogspot.com/-nY7RCflMJOk/TdVR-JHjEyI/AAAAAAAAAC8/D0tVTHeksow/s1600/Powerman_5000_umvd01.jpg"/></div>');
+// server.use('/', (req, res) => {
+//     res.status(404).send('<div style="padding:15% 0 5%;background-color:black;display: flex;flex-direction: column;align-items:center;"><h1 style="color:lawngreen;font-size:46px;">404 could not find page</h1><img src="http://3.bp.blogspot.com/-nY7RCflMJOk/TdVR-JHjEyI/AAAAAAAAAC8/D0tVTHeksow/s1600/Powerman_5000_umvd01.jpg"/></div>');
+// });
+
+server.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    res.sendFile(filePath);
 });
 
 module.exports = server;
@@ -21,12 +27,4 @@ module.exports = server;
 function logger(req, res, next){
     console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`);
     next();
-}
-
-function auth(req, res, next){
-    if(req.url === '/mellon'){
-        next()
-    } else {
-        res.send('You shall not pass!')
-    }
 }
