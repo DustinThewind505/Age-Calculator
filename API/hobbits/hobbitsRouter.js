f567aa6a2cb9292ee131f4be63abc3b78e7e12eb
unknown <Dustin.Guillen14@yahoo.com>
2020-06-21 10:23:50 -0600

    used knex on hobbits router

--- API/hobbits/hobbitsRouter.js
+++ API/hobbits/hobbitsRouter.js
@@ -1,40 +1,46 @@
 const express = require('express');
 
+const Hobbits = require('../../data/dbHelper');
+
 const router = express.Router();
 
-let hobbits = [
-    {
-      id: 1,
-      name: 'Bilbo Baggins',
-      age: 111,
-    },
-    {
-      id: 2,
-      name: 'Frodo Baggins',
-      age: 33,
-    },
-  ];
+// let hobbits = [
+//     {
+//       id: 1,
+//       name: 'Bilbo Baggins',
+//       age: 111,
+//     },
+//     {
+//       id: 2,
+//       name: 'Frodo Baggins',
+//       age: 33,
+//     },
+//   ];
   let nextId = 3;
 
 
 router.get('/', (req, res) => {
-    res.status(200).json(hobbits)
+    Hobbits.findHobbits()
+    .then(hobbits => {
+        res.status(200).json(hobbits)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "ERROR failed to get hobbit", errorMessage: err })
+    })
 })
 
 router.get('/:id', (req, res) => {
-const hobbit = hobbits.find(h => h.id == req.params.id)
-
-    res.status(200).json(hobbit)
-})
-
-router.post('/', (req, res) => {
-    const hobbit = req.body;
-
-    hobbit.id = nextId++;
-
-    hobbits.push(hobbit);
-
-    res.status(201).json(hobbits)
+    Hobbits.findHobbitById(req.params.id)
+    .then(hobbit => {
+        if(hobbit){
+            res.status(200).json(hobbit)
+        } else {
+            res.status(404).json({ message: "hobbit not found" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "ERROR failed to get hobbit", errorMessage: err })
+    })
 })
 
 router.put('/:id', (req, res) => {
--- data/dbHelper.js
+++ data/dbHelper.js
@@ -1,4 +1,19 @@
 const db = require('./dbConfig');
 
+module.exports = {
+    // add,
+    findHobbits,
+    findHobbitById
+}
+
+// =========== GET Hobbits ===========
+function findHobbits() {
+    return db('hobbits')
+}
+
+// =========== GET Hobbits by id ===========
+function findHobbitById(id) {
+    return db('hobbits').where({id})
+}
 
 
